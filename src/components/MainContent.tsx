bc214f7360f66657b032b9f43e3095876c3a53b3
mihghost <hellenmichelee@hotmail.com>
2022-07-15 12:58:48 -0300

    last alterations

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,37 +1,54 @@
 import LargeCard from "./LargeCard";
 import SmallCard from "./SmallCard";
+import SideBar from "./SideBar";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from "recharts";
+import * as React from "react";
+import Paper from "@mui/material/Paper";
+import {
+  Chart,
+  BarSeries,
+  Title,
+  ArgumentAxis,
+  ValueAxis,
+} from "@devexpress/dx-react-chart-material-ui";
+import { Animation } from "@devexpress/dx-react-chart";
 
 const MainContent = () => {
+  const [post, setPost] = useState<any[]>([{}]);
+  const [count, setCount] = useState(0);
+  const [slug, setSlug] = useState<string>();
+  const baseURL = `http://testess.atwebpages.com/api.php?n=10`;
+
+  const chartData = [
+    { year: "1950", population: 2.525 },
+    { year: "1960", population: 3.018 },
+    { year: "1970", population: 3.682 },
+    { year: "1980", population: 4.44 },
+    { year: "1990", population: 5.31 },
+    { year: "2000", population: 6.127 },
+    { year: "2010", population: 6.93 },
+  ];
+  const apiData: any = [];
+
+  const increment = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+  useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+    });
+  }, [count]);
+  useEffect(() => {
+    const interval = setInterval(increment, 100000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="text-gray-150 p-10 flex-grow">
-      <div className="space-x-3 text-right">
-        <button className="bg-gray-150 rounded-full w-10 h-10 text-darkblue font-bold text-xl">
-          &deg;C
-        </button>
-        <button className="bg-[#585676] rounded-full w-10 h-10 text-gray-150 font-bold text-xl">
-          &deg;F
-        </button>
-      </div>
-
-      <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-5 my-5 gap-10 justify-center">
-        <SmallCard
-          dayTitle="Tomorrow"
-          img="Shower"
-          max={20}
-          min={12}
-          temp="C"
-        />
-        <SmallCard
-          dayTitle="Sun, 7 Jun"
-          img="Clear"
-          max={27}
-          min={18}
-          temp="C"
-        />
-      </div>
-
       <div className="my-10">
-        <h3 className="text-2xl font-bold mb-5">Today's Highlights</h3>
+        <h3 className="text-2xl font-bold mb-5">Clima hoje</h3>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 justify-center">
           <LargeCard title="Wind Status" num={7} desc="mph">
             <div className="flex justify-between space-x-5 items-center">
@@ -42,7 +59,11 @@ const MainContent = () => {
             </div>
           </LargeCard>
 
-          <LargeCard title="Humidity" num={84} desc="%">
+          <LargeCard
+            title="Umidade"
+            num={post[count] === undefined ? "ops" : post[count].umidade}
+            desc="%"
+          >
             <div className="self-stretch text-gray-250 text-xs space-y-1">
               <div className="flex justify-between space-x-5 items-center px-1">
                 <p>0</p>
@@ -52,20 +73,15 @@ const MainContent = () => {
               <div className="w-full h-2 bg-gray-150 rounded-full overflow-hidden">
                 <div
                   className="bg-[#FFEC65] h-2"
-                  style={{ width: "84%" }}
+                  style={{ width: `${post[count].umidade}%` }}
                 ></div>
               </div>
               <p className="text-right">%</p>
             </div>
           </LargeCard>
-
-          <LargeCard title="Visibility" num={6.4} desc=" miles" />
-
-          <LargeCard title="Air Pressure" num={998} desc=" mb" />
         </div>
       </div>
     </div>
   );
 };
-
 export default MainContent;
